985cec44e2e78dc404710eea68ce7796325433bf
Eduardo Garcia Bautista <eduardo.garciab@hormail.com>
2023-09-08 16:05:36 -0600

    Add integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MeComponent } from './pages/me/me.component';
+import { FinalComponent } from './pages/final/final.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'me',
+    component: MeComponent
+  },
+  {
+    path: 'final',
+    component: FinalComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'welcome'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MeComponent } from './pages/me/me.component';
+import { FinalComponent } from './pages/final/final.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MeComponent,
+    FinalComponent
   ],
   imports: [
     BrowserModule,
